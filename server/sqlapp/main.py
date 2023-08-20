4530d51facebd66772b0d33f9383ed237f5fc093
jainaryan <jain.aryan.r@gmail.com>
2023-08-20 09:41:00 +0530

    Removed unused code

--- server/sqlapp/main.py
+++ server/sqlapp/main.py
@@ -5,14 +5,17 @@ from starlette.requests import Request
 from starlette.responses import HTMLResponse
 from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
+from fastapi import Depends, FastAPI, HTTPException, Header
+from sqlalchemy.orm import Session
 
+from typing import Annotated
 import models, schemas
 from sqlapp import crud
 from sqlapp.database import SessionLocal, engine
 
 models.base.metadata.create_all(bind=engine)
 
-#ryj for testing
+# ryj for testing
 from data.fake_post import fake_posts_db
 
 app = FastAPI()
@@ -30,14 +33,11 @@ def get_db():
         db.close()
 
 
-@app.get('/index.html', response_class=HTMLResponse)
-def get_index():
-    pass
-
 @app.get('/blog.html', response_class=HTMLResponse)
 def get_blog(request: Request):
     return templates.TemplateResponse("blog.html", {"request": request,
                                                     "posts": fake_posts_db})
+
 
 @app.get('/gardens/{g_id}', response_model=schemas.Garden)
 def find_garden(g_id: int, db: Session = Depends(get_db)):
@@ -46,10 +46,12 @@ def find_garden(g_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="garden not found")
     return garden_found
 
+
 @app.get('/gardens/findstate', response_model=list[int])
-def findstate(state:str, db: Session = Depends(get_db)):
+def findstate(state: str, db: Session = Depends(get_db)):
     entry_ids = crud.get_state(db, 'assam')
     return entry_ids
 
+
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="localhost", port=7000)
